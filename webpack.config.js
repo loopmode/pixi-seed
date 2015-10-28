71bc9876cd0173918799db60dcb5ecaa1fdcd242
Jovica <jovica.aleksic@loopmode.de>
2015-10-28 22:39:08 +0100

    implemented webpack hot loading from https://github.com/christianalfoni/webpack-express-boilerplate changed config to run on windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,60 @@
 var path = require('path');
-var pkg = require('./package.json')
+var pkg = require('./package.json');
+var webpack = require('webpack');
 var DEBUG = process.env.NODE_ENV !== 'production';
-var util = require('util');
-var entry = {
-  app: ['./app.js']
-};
-
+var buildDir = path.resolve(pkg.config.buildDir);
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    context: path.join(__dirname, 'app'),
-    entry: entry,
-    debug : DEBUG,
-    target : 'web',
-    devtool : DEBUG ? 'inline-source-map' : false,
+    //context: path.join(__dirname, 'app'),
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+        path.join(__dirname, 'src/app.js')
+    ],
+    debug: DEBUG,
+    target: 'web',
+    devtool: DEBUG ? 'inline-source-map' : false,
     output: {
-        path: path.resolve(pkg.config.buildDir),
-        publicPath: DEBUG ? "/" : "./",
-        filename: "bundle.js"
+        path: buildDir,
+        publicPath: '/',
+        filename: '[name.js'
     },
     node: {
-      fs: 'empty'
+        fs: 'empty'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+            filename: 'index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
     module: {
-        loaders: [
-          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-          { test: /\.html$/, exclude: /node_modules/, loader: "file-loader?name=[path][name].[ext]"},
-          { test: /\.jpe?g$|\.svg$|\.png$/, exclude: /node_modules/, loader: "file-loader?name=[path][name].[ext]"},
-          { test: /\.json$/, exclude: /node_modules/, loader: "json"},
-          {
-          test: /\.json$/,
-          include: path.join(__dirname, 'node_modules', 'pixi.js'),
-          loader: 'json',
-          }
-        ]
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }, {
+            test: /\.html$/,
+            exclude: /node_modules/,
+            loader: 'file?name=[path][name].[ext]'
+        }, {
+            test: /\.jpe?g$|\.svg$|\.png$/,
+            exclude: /node_modules/,
+            loader: 'file?name=[path][name].[ext]'
+        }, {
+            test: /\.json$/,
+            exclude: /node_modules/,
+            loader: 'json'
+        }, {
+            test: /\.json$/,
+            include: path.join(__dirname, 'node_modules', 'pixi.js'),
+            loader: 'json',
+        }]
     }
 };
